4c05e22b05972898291f7f4e950d580e6988773b
SHAHUL-AHMED-77 <shahul77235@gmail.com>
2026-06-07 14:24:42 +0530

    Add : UserSchema model

---
+++ src/models/User.ts
@@ -0,0 +1,64 @@
+import { model, models, Schema } from "mongoose";
+
+
+export type UserRole = "member" | "moderator" | "admin" | "owner" | "super_admin"
+
+export interface IUser extends Document {
+    username: string,
+    email: string,
+    passowrd: string,
+    avatar?: string,
+    bio?: string,
+    role: UserRole,
+    lastLogin?: Date,
+}
+
+const UserSchema = new Schema<IUser>({
+    username: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 3,
+        maxLength: 30,
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+        lowercase: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    avatar: {
+        type: String,
+        default: "",
+    },
+    bio: {
+        type: String,
+        default: "",
+        maxlength: 300,
+    },
+    role: {
+        type: String,
+        enum: [
+            "member",
+            "moderator",
+            "admin",
+            "owner",
+            "super_admin",
+        ],
+        default: "member"
+    },
+    lastLogin: Date,
+},
+    {
+        timestamps: true,
+    })
+
+const User = models.User || model<IUser>("User", UserSchema)
+
+export default User;
+
