4b7a2a718dfbdf469de41079603b4e47d4abfa9f
SHAHUL-AHMED-77 <shahul77235@gmail.com>
2026-06-12 12:34:20 +0530

    Add : Membership  Schema Model

---
+++ src/models/Membership.ts
@@ -0,0 +1,52 @@
+import mongoose, { Document, Schema } from "mongoose";
+import { CommunityRole } from "@/constants/community-role";
+
+export interface IMembership extends Document {
+  user: mongoose.Types.ObjectId;
+  community: mongoose.Types.ObjectId;
+  role: CommunityRole;
+}
+
+const MembershipSchema = new Schema<IMembership>(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    community: {
+      type: Schema.Types.ObjectId,
+      ref: "Community",
+      required: true,
+    },
+
+    role: {
+      type: String,
+      enum: Object.values(CommunityRole),
+      default: CommunityRole.MEMBER,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+MembershipSchema.index(
+  {
+    user: 1,
+    community: 1,
+  },
+  {
+    unique: true,
+  }
+);
+
+const Membership =
+  mongoose.models.Membership ||
+  mongoose.model<IMembership>(
+    "Membership",
+    MembershipSchema
+  );
+
+export default Membership;
