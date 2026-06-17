84458ac72cf684a5a5e3fb026ee68e5e99020a47
SHAHUL-AHMED-77 <shahul77235@gmail.com>
2026-06-17 14:12:49 +0530

    Add : Default Community Boards

---
+++ src/constants/defaultBoards.ts
@@ -0,0 +1,22 @@
+export const DEFAULT_BOARDS = [
+  {
+    name: "Announcements",
+    slug: "announcements",
+  },
+  {
+    name: "General",
+    slug: "general",
+  },
+  {
+    name: "Questions",
+    slug: "questions",
+  },
+  {
+    name: "Projects",
+    slug: "projects",
+  },
+  {
+    name: "Resources",
+    slug: "resources",
+  },
+];
